843bb1b87bfebb0e42546b4d68b429b6bcf99262
kumarpritam1468 <manoharipritam@gmail.com>
2024-04-05 19:07:35 +0530

    add hero content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ButtonGradient from './assets/svg/ButtonGradient'
-import Button from "./components/Button";
+// import Button from "./components/Button";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Router>
           <div className=" pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
             <Header/>
+            <Hero/>
           </div>
           <ButtonGradient />
       </Router>
