06429d87c65cd8d83ae942b049acaf8ebf2b14d0
kumarpritam1468 <manoharipritam@gmail.com>
2024-04-06 17:07:54 +0530

    add hero parallax

---
+++ src/components/CompanyLogos.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { companyLogos } from '../constants'
+
+const CompanyLogos = ({className}) => {
+  return (
+    <div className={className}>
+        <h5 className=' text-center'>
+            Helping People Create Beautiful Content at
+        </h5>
+        <ul className=' flex'>
+            {companyLogos.map((logo, index)=> (
+                <li key={index} className=' flex items-center justify-center flex-1 h-[8.5rem]'>
+                    <img src={logo} alt={logo} width={134} height={28}  />
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default CompanyLogos
---
+++ src/components/Generating.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { file02, loading } from '../assets'
+
+const Generating = ({className}) => {
+  return (
+    <div className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${className || ''} text-base`}>
+        <img src={loading} alt="loading" className=' w-5 h-5 mr-4' />
+        Ai is Generating
+    </div>
+  )
+}
+
+export default Generating
