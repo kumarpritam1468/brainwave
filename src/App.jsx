9dc2ac7f4bf0451465f8b6c744435801a5aa2528
kumarpritam1468 <manoharipritam@gmail.com>
2024-04-09 16:20:10 +0530

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
             <Services/>
             <Pricing />
             <Roadmap/>
+            <Footer/>
           </div>
           <ButtonGradient />
       </Router>
